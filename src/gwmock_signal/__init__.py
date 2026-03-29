e804cbd74a0034493bb592f6efa0e1bd96091e80
Isaac C. F. Wong <23655285+isaac-cf-wong@users.noreply.github.com>
2026-03-29 15:41:02 +0200

    feat: Add support for custom detector geometries (#54)
    * feat: Add support for custom detector geometries
    
    * fix: Use internal key to avoid name collision
    
    * chore: Validate name, azimuth and tilt

--- src/gwmock_signal/__init__.py
+++ src/gwmock_signal/__init__.py
@@ -2,8 +2,9 @@
 
 from __future__ import annotations
 
+from gwmock_signal.detector import CustomDetector
 from gwmock_signal.network import Network
 from gwmock_signal.simulator import CBCSimulator, GWSimulator, TransientSimulator
 from gwmock_signal.version import __version__
 
-__all__ = ["CBCSimulator", "GWSimulator", "Network", "TransientSimulator", "__version__"]
+__all__ = ["CBCSimulator", "CustomDetector", "GWSimulator", "Network", "TransientSimulator", "__version__"]
